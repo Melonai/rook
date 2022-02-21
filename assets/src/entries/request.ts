456d2d49358356b5895ddb71c5895d3eabb7e677
Mel <einebeere@gmail.com>
2022-02-21 01:57:38 +0100

    Register handler to show toasts for all errors

--- assets/src/entries/request.ts
+++ assets/src/entries/request.ts
@@ -2,6 +2,22 @@ import RequestPage from "../components/RequestPage.svelte";
 import { RookType } from "../models/rook_type";
 import { setClientType } from "../state/constant_state";
 import { initializeRequest } from "../state/request";
+import { toast, ToastType } from "../state/toast";
+
+window.addEventListener("load", () => {
+    function onError(message: string) {
+        toast({
+            type: ToastType.ERROR,
+            title: "An error occurred!",
+            message: message,
+        });
+    }
+
+    window.addEventListener("error", e => onError(e.message));
+    window.addEventListener("unhandledrejection", e => {
+        onError(e.reason.message ?? e.reason ?? "Unknown error");
+    });
+});
 
 setClientType(RookType.REQUEST);
 initializeRequest();
--- assets/src/entries/share.ts
+++ assets/src/entries/share.ts
@@ -2,6 +2,22 @@ import SharePage from "../components/SharePage.svelte";
 import { RookType } from "../models/rook_type";
 import { setClientType } from "../state/constant_state";
 import { initializeShare } from "../state/share";
+import { toast, ToastType } from "../state/toast";
+
+window.addEventListener("load", () => {
+    function onError(message: string) {
+        toast({
+            type: ToastType.ERROR,
+            title: "An error occurred!",
+            message: message,
+        });
+    }
+
+    window.addEventListener("error", e => onError(e.message));
+    window.addEventListener("unhandledrejection", e => {
+        onError(e.reason.message ?? e.reason ?? "Unknown error");
+    });
+});
 
 setClientType(RookType.SHARE);
 initializeShare();
